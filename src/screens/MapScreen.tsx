5252dd6920b4f2aa784e3ce8bb266d49c98368bc
Hamidah Ismail 🍟 <hamidah@socar.my>
2023-02-04 15:06:00 +0800

    Adding RNMaps

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,39 +1,46 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
+  View,
   StatusBar,
   useColorScheme,
-  Text
+  Text,
+  StyleSheet,
 } from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import Icon from 'react-native-vector-icons/AntDesign';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 function MapScreen(): JSX.Element {
-    const isDarkMode = useColorScheme() === 'dark';
-  
-    const backgroundStyle = {
-      backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-  
-    return (
-      <SafeAreaView style={backgroundStyle}>
-        <StatusBar
-          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-          backgroundColor={backgroundStyle.backgroundColor}
-        />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Text>Map Screen</Text>
-          <Icon name="infocirlce" size={30} color="green" />
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
+  const isDarkMode = useColorScheme() === 'dark';
 
-  export default MapScreen
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Text>Map Screen</Text>
+      <Icon name="infocirlce" size={30} color="green" />
+
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={StyleSheet.absoluteFillObject}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
+    </SafeAreaView>
+  );
+}
+
+export default MapScreen;
